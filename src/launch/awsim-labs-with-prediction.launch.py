4e029a3df8b442b76a64fbb40a462fa11783551d
kazushi67 <osushi.115667@gmail.com>
2024-11-25 11:57:51 +0900

    feat(awsim-labs-with-prediction.launch.py): add awsim-labs-with-prediction launch file
    Signed-off-by: kazushi67 <osushi.115667@gmail.com>

---
+++ src/launch/awsim-labs-with-prediction.launch.py
@@ -0,0 +1,88 @@
+# Copyright 2024 Tier IV, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from ament_index_python.packages import get_package_share_directory
+import launch
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+import yaml
+
+def generate_launch_description():
+    launch_arguments = []
+    def add_launch_arg(name: str, default_value=None, description=None):
+        return  launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
+
+    #set prediction-node
+    map_based_prediction_param_file = os.path.join(
+        get_package_share_directory("awsim_map_based_prediction"), "config/map_based_prediction.param.yaml"
+    )
+    with open(map_based_prediction_param_file, "r") as f:
+        map_based_prediction_param = yaml.safe_load(f)["/**"]["ros__parameters"]
+    prediction_Node = Node(
+        package="awsim_map_based_prediction",
+        namespace="AWSIM",
+        executable="map_based_prediction",
+        name="map_based_prediction",
+        output="both",
+        parameters=[map_based_prediction_param],
+        remappings=[
+            ('/vector_map','map/vector_map'),
+            ('/traffic_signals','perception/traffic_light_recognition/traffic_signals'),
+            ('~/output/objects','perception/object_recognition/objects'),
+            ('~/input/objects','perception/object_recognition/tracking/objects')
+        ]
+    )
+
+    # set map-lodaer-node
+    lanelet2_map_loader_param_file = os.path.join(
+        get_package_share_directory("awsim_map_loader"), "config/lanelet2_map_loader.param.yaml"
+    )
+    with open(lanelet2_map_loader_param_file, "r") as f:
+        lanelet2_map_loader_param = yaml.safe_load(f)["/**"]["ros__parameters"]
+    add_launch_arg('lanelet2_map_path', '', 'path to lanelet2_map.osm')
+    map_lodaer_Node = Node(
+        package="awsim_map_loader",
+        namespace="AWSIM",
+        executable="lanelet2_map_loader",
+        name="lanelet2_map_loader",
+        output="both",
+        parameters=[
+            lanelet2_map_loader_param,
+            {
+                'lanelet2_map_path': LaunchConfiguration('lanelet2_map_path'),
+            }
+        ],
+        remappings=[
+            ('output/lanelet2_map','map/vector_map')
+        ]
+    )
+
+    # set AWSIM
+    awsim_with_prediction = ExecuteProcess(
+        cmd=[[
+            LaunchConfiguration('AWSIM_path')
+        ]],
+        shell=True
+    )
+
+    return launch.LaunchDescription(
+        [
+        prediction_Node,
+        map_lodaer_Node,
+        awsim_with_prediction,
+        ]
+    )
+
