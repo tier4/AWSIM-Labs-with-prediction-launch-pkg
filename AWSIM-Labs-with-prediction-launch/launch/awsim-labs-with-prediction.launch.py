ffe273343b170e0f49d16ab584a617bcbd760a28
kazushi67 <osushi.115667@gmail.com>
2024-12-11 17:35:48 +0900

    fix(awsim-labs-with-prediction.launch.py): fix package-name
    Signed-off-by: kazushi67 <osushi.115667@gmail.com>

--- AWSIM-Labs-with-prediction-launch/launch/awsim-labs-with-prediction.launch.py
+++ AWSIM-Labs-with-prediction-launch/launch/awsim-labs-with-prediction.launch.py
@@ -24,12 +24,12 @@ def generate_launch_description():
 
     #set prediction-node
     map_based_prediction_param_file = os.path.join(
-        get_package_share_directory("awsim_map_based_prediction"), "config/map_based_prediction.param.yaml"
+        get_package_share_directory("autoware_map_based_prediction"), "config/map_based_prediction.param.yaml"
     )
     with open(map_based_prediction_param_file, "r") as f:
         map_based_prediction_param = yaml.safe_load(f)["/**"]["ros__parameters"]
     prediction_Node = Node(
-        package="awsim_map_based_prediction",
+        package="autoware_map_based_prediction",
         namespace="AWSIM",
         executable="map_based_prediction",
         name="map_based_prediction",
@@ -45,12 +45,12 @@ def generate_launch_description():
 
     # set map-loader-node
     lanelet2_map_loader_param_file = os.path.join(
-        get_package_share_directory("awsim_map_loader"), "config/lanelet2_map_loader.param.yaml"
+        get_package_share_directory("map_loader"), "config/lanelet2_map_loader.param.yaml"
     )
     with open(lanelet2_map_loader_param_file, "r") as f:
         lanelet2_map_loader_param = yaml.safe_load(f)["/**"]["ros__parameters"]
     map_loader_Node = Node(
-        package="awsim_map_loader",
+        package="map_loader",
         namespace="AWSIM",
         executable="lanelet2_map_loader",
         name="lanelet2_map_loader",
@@ -69,14 +69,14 @@ def generate_launch_description():
 
     # set map-projection-node
     map_projection_loader_param_file = os.path.join(
-        get_package_share_directory("awsim_map_projection_loader"), "config/map_projection_loader.param.yaml"
+        get_package_share_directory("map_projection_loader"), "config/map_projection_loader.param.yaml"
     )
     with open(map_projection_loader_param_file, "r") as f:
         map_projection_loader_param = yaml.safe_load(f)["/**"]["ros__parameters"]
     map_projection_Node = Node(
-        package="awsim_map_projection_loader",
+        package="map_projection_loader",
         namespace="AWSIM",
-        executable="awsim_map_projection_loader_node",
+        executable="map_projection_loader_node",
         name="map_projection_loader",
         output="both",
         parameters=[
